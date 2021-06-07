b58293f5c2de3b48470fc3be165c21628bec3e2c
hiya-debug <75578766+hiya-debug@users.noreply.github.com>
2021-06-07 10:17:50 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+function setup() {
+  var car,wall;
+  var speed,wight;
+  speed = random(55,90)
+  wight = random(400,1500)
+  car = createSprite(50,200,50,50)
+  wall = createSprite(1500,200,60,hight/2)
+  car.velocityX = speed
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);
+if(wall.x-car.x < (car.wight+wall.wight)/2)
+{
+  car.velocityX = 0
+  var deformation = 0.5 * wight * speed* speed/22509;
+  if(deformation > 180)
+  {
+    car.shapeColor = color(255,0,0)
+  }
+  if(deformation > 180 && deformation>100)
+  {
+    car.shapeColor = color(0,255,0)
+  }
+}
+  drawSprites();
+}
